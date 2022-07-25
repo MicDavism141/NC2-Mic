74cf82619e99f6282a837990c8f66674fb0cb7ec
Micah Davis <micah.davis@ui.ac.id>
2022-07-25 12:04:01 +0700

    Initial Commit

---
+++ ToLearnList1/ToLearnList1App.swift
@@ -0,0 +1,17 @@
+//
+//  ToLearnList1App.swift
+//  ToLearnList1
+//
+//  Created by Micah Davis on 25/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ToLearnList1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
