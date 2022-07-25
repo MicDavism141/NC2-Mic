1568d7e17372e3277fcf268aefef4e55dc8516fc
Micah Davis <micah.davis@ui.ac.id>
2022-07-25 12:33:03 +0700

    initial project, basic layouting

--- ToLearnList1/ContentView.swift
+++ ToLearnList1/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var ThingsToLearn = ""
+    let paddingSize = 15.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("Things to learn")
+                    .bold()
+//                    .padding
+//                    .font(<#T##font: Font?##Font?#>)
+                TextField("Learn XCode Insert Icon", text: $ThingsToLearn)
+                Text("Learning Format")
+                HStack {
+                    Button(action: {
+                    }) {
+                        Text("Personal")
+                    }
+                    Button(action: {
+                    }) {
+                        Text("Personal")
+                    }
+                }
+                Text("Priority Level")
+                HStack {
+                    Button(action: {
+                    }) {
+                        Text("Personal")
+                    }
+                    Button(action: {
+                    }) {
+                        Text("Personal")
+                    }
+                }
+                Spacer()
+            }
+            .navigationTitle("Learning Objective")
+            
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
